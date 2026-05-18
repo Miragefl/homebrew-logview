bb78575903d92301a24c0d40499c22cbbf7ad6d3
fenglei <silence_fl@yeah.net>
2026-05-18 18:20:08 +0800

    bump: fix version injection in v0.8.0 binaries

--- Formula/logview.rb
+++ Formula/logview.rb
@@ -9,7 +9,7 @@ class Logview < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/Miragefl/logview/releases/download/v0.8.0/logview-darwin_amd64.tar.gz"
-      sha256 "e0a65a39fd14ac6f169728ce8f4c9fb62ff47b46252f6d0401b31d4f4860780a"
+      sha256 "7f7a894f864d63e5e439c2585984efdacb5fc4e96ea09e3431dd9654bd239d63"
 
       define_method(:install) do
         bin.install "logview-darwin_amd64" => "logview"
@@ -17,7 +17,7 @@ class Logview < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/Miragefl/logview/releases/download/v0.8.0/logview-darwin_arm64.tar.gz"
-      sha256 "de5ce9cbfbc0738d84826837dacfbc51f89748215e4deaf497f6b28856b99073"
+      sha256 "9da189470e5488b738df3938f1a3749d26b038646738c0971ffef3952dacbcb8"
 
       define_method(:install) do
         bin.install "logview-darwin_arm64" => "logview"
@@ -28,14 +28,14 @@ class Logview < Formula
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/Miragefl/logview/releases/download/v0.8.0/logview-linux_amd64.tar.gz"
-      sha256 "b0e4f87c4776e5f60f63446d2c5c7cca4db4935c47959911c7b42b7d8b147c49"
+      sha256 "ebf723652c2c7dba96a6c5364f95faed2c1d7f10c1de1f022fbc155e8af6f034"
       define_method(:install) do
         bin.install "logview-linux_amd64" => "logview"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/Miragefl/logview/releases/download/v0.8.0/logview-linux_arm64.tar.gz"
-      sha256 "da4b593b51ad4f474053435de3d9162e277cccfe890e5688e8ee6ce8db42178a"
+      sha256 "21e9c9cd6b1c85fcd903d9a4a9dcbbdb29c07900707241a51abfb5615aaa8dc7"
       define_method(:install) do
         bin.install "logview-linux_arm64" => "logview"
       end
