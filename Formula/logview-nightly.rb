53a1faac124db711d6ae9f29064478f3b2819000
fenglei <silence_fl@yeah.net>
2026-05-20 14:39:34 +0800

    nightly: 0.10.0-nightly-20260520-063917

--- Formula/logview-nightly.rb
+++ Formula/logview-nightly.rb
@@ -4,12 +4,12 @@
 class LogviewNightly < Formula
   desc "Terminal log viewer (nightly build)"
   homepage "https://github.com/Miragefl/logview"
-  version "0.10.0-nightly-20260520-062639"
+  version "0.10.0-nightly-20260520-063917"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/Miragefl/logview/releases/download/nightly/logview-darwin_amd64.tar.gz"
-      sha256 "8cebf609069b48264f04ceac9297e7d865517eb97fe878bf9130a7fe0e85cd8d"
+      sha256 "97bcb263afd090d4b83e691732eeab3cbed7545c479ae03cefa231deb30ee4eb"
 
       define_method(:install) do
         bin.install "logview-darwin_amd64" => "logview"
@@ -17,7 +17,7 @@ class LogviewNightly < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/Miragefl/logview/releases/download/nightly/logview-darwin_arm64.tar.gz"
-      sha256 "eae43c43884d65752ab5ebdf6f8590b4e32272b3e06a067b728b9a58856f3c2d"
+      sha256 "e95bb1ade02712527a44b3b3e3e99c09bb31ebb1f7e644a31a896afe8332da16"
 
       define_method(:install) do
         bin.install "logview-darwin_arm64" => "logview"
@@ -28,14 +28,14 @@ class LogviewNightly < Formula
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/Miragefl/logview/releases/download/nightly/logview-linux_amd64.tar.gz"
-      sha256 "a37a8ff4c4f9bea5524396e0dda2b47fa880b87f520acd537883e3e90abc2fb9"
+      sha256 "97c07aeaa9f1ab43ec552993671ee4e88e11b45ac37adf2463694335029348f3"
       define_method(:install) do
         bin.install "logview-linux_amd64" => "logview"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/Miragefl/logview/releases/download/nightly/logview-linux_arm64.tar.gz"
-      sha256 "9f12bd2cace912d35ae29a6f8c3585323ca2dfc9a35569fe5c97662c47931877"
+      sha256 "406651c5071b20c752e1741491b9053293d6a179c8733a18fcdd7541f72d42e4"
       define_method(:install) do
         bin.install "logview-linux_arm64" => "logview"
       end
